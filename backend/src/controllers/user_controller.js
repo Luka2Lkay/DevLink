32ee52ea0f6468d75e06e6555339d3d151344857
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-06 11:16:31 +0000

    create signUp controller function

---
+++ backend/src/controllers/user_controller.js
@@ -0,0 +1,38 @@
+const User = require("../models/user_model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { secretKey } = require("../config/auth_key_config");
+
+const signUp = async (req, res) => {
+  const { name, email, password, confirmPassword, githubUsername } = req.body;
+
+  const hash = bcrypt.hashSync(password, 10);
+  const hash2 = bcrypt.hashSync(confirmPassword, 10);
+
+  const checkEmail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g.test(email);
+
+  try {
+    const user = new User({
+      name,
+      email,
+      password: hash,
+      confirmPassword: hash2,
+      githubUsername,
+    });
+
+    if (password !== confirmPassword) {
+      return res.status(400).send("Passwords don't match!");
+    }
+
+    if (password === confirmPassword && checkEmail) {
+      registeredUser = await user.save();
+      res
+        .status(201)
+        .json({ message: "Registered sucessfully!", user: registeredUser });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = { signUp };
