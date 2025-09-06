fb02e9f2db2b845e3dd92e9ac71a5489d6f3c8e5
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-06 10:34:38 +0000

    create user model

---
+++ backend/src/models/user_model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: false },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  confirmPassword: { type: String, required: true },
+  githubUsername: { type: String, required: true, unique: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
