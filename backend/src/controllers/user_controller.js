ca850897a7ab410ef088eb47dc6c0d2038bb6ca1
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-09 07:44:18 +0000

    add more controllers and routes

--- backend/src/controllers/user_controller.js
+++ backend/src/controllers/user_controller.js
@@ -6,11 +6,12 @@ const { secretKey } = require("../config/auth_key_config");
 const signUp = async (req, res) => {
   const { name, email, password, confirmPassword, githubUsername } = req.body;
 
-  return res.status(200).send("hey")
   const hash = bcrypt.hashSync(password, 10);
   const hash2 = bcrypt.hashSync(confirmPassword, 10);
 
   const checkEmail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g.test(email);
+
+  const githubUrl = `https://api.github.com/users/${githubUsername}`;
 
   try {
     const user = new User({
@@ -18,14 +19,14 @@ const signUp = async (req, res) => {
       email,
       password: hash,
       confirmPassword: hash2,
-      githubUsername,
+      githubUsername: githubUrl,
     });
 
     if (password !== confirmPassword) {
       return res.status(400).send("Passwords don't match!");
     }
 
-    if (password === confirmPassword && checkEmail) {
+    if (password === confirmPassword && checkEmail && githubUsername) {
       registeredUser = await user.save();
       res
         .status(201)
@@ -36,4 +37,60 @@ const signUp = async (req, res) => {
   }
 };
 
-module.exports = { signUp };
+const deleteAllUsers = async (req, res) => {
+  try {
+    await User.deleteMany({});
+
+    res.status(200).json({ message: "Successfully deleted all users!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getOneUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (user) {
+      res.status(200).json({ user });
+    } else {
+      res.status(404).json({ message: "User not found!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteOneUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const deleteUser = await User.findByIdAndDelete(userId);
+
+    if (deleteUser) {
+      res.status(200).json({ message: "The user is successfully deleted!" });
+    } else {
+      res.status(404).json({ message: "User not found!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  signUp,
+  deleteAllUsers,
+  deleteOneUser,
+  getAllUsers,
+  getOneUser,
+};
--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const {signUp} = require("../controllers/user_controller")
+const {signUp, deleteAllUsers, deleteOneUser, getAllUsers, getOneUser} = require("../controllers/user_controller")
 
 const userRoutes = (app) => {
 
 const router = express.Router();
 
 router.post("/add-user", signUp);
+router.delete("/delete-user/:id", deleteOneUser);
+router.delete("/delete-all-users", deleteAllUsers);
+router.get("/get-all-users", getAllUsers);
+router.get("/get-user/:id", getOneUser);
 
 app.use("/api/users", router);
-
 };
 
 module.exports = {userRoutes};
