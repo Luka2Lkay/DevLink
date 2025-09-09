c787c2a1d4e7372e84ac462550e59206d6fd421c
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-09 16:17:24 +0000

    get projects by user

--- backend/src/controllers/project_controller.js
+++ backend/src/controllers/project_controller.js
@@ -1,5 +1,6 @@
 const Project = require("../models/project_model");
 const { validationResult } = require("express-validator");
+const User = require("../models/user_model");
 
 const createProject = async (req, res) => {
   const errors = validationResult(req);
@@ -84,6 +85,28 @@ const updateProject = async (req, res) => {
   }
 };
 
+const projectsByUserId = async (req, res) => {
+  const userId = req.params.id;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  try {
+    const projects = await Project.find();
+
+    const userProjects = projects.filter((project) => {
+      if (project.owner == userId) {
+        return project;
+      }
+    });
+    res.status(200).json({ userProjects });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   createProject,
   deleteOneProject,
@@ -91,4 +114,5 @@ module.exports = {
   getOneProject,
   getAllProjects,
   updateProject,
+  projectsByUserId,
 };
--- backend/src/routes/project_routes.js
+++ backend/src/routes/project_routes.js
@@ -6,6 +6,7 @@ const {
   getOneProject,
   getAllProjects,
   updateProject,
+  projectsByUserId,
 } = require("../controllers/project_controller");
 const { verifyToken } = require("../middleware/verify");
 const { validateCreateProject } = require("../middleware/validation");
@@ -24,6 +25,7 @@ const projectRoutes = (app) => {
   router.get("/get-project/:id", getOneProject);
   router.get("/get-all-projects", getAllProjects);
   router.put("/update-project/:id", updateProject);
+  router.get("/projects-by-user/:id", projectsByUserId);
 
   app.use("/api/projects", router);
 };
