d8bf85229369f9013bdd7a2b85900bc6d5c9e813
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-09 12:15:29 +0000

    write a get all projects route

--- backend/src/controllers/project_controller.js
+++ backend/src/controllers/project_controller.js
@@ -11,6 +11,7 @@ const createProject = async (req, res) => {
       collaborators,
     });
 
+    project.save();
     res.status(201).json({ message: "Project successfully created!", project });
   } catch (error) {
     res.status(500).send(error.message);
@@ -53,9 +54,20 @@ const getOneProject = async (req, res) => {
   }
 };
 
+const getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+
+    res.status(200).json({ projects });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   createProject,
   deleteOneProject,
   deleteAllProjects,
   getOneProject,
+  getAllProjects,
 };
--- backend/src/routes/project_routes.js
+++ backend/src/routes/project_routes.js
@@ -3,6 +3,8 @@ const {
   createProject,
   deleteOneProject,
   deleteAllProjects,
+  getOneProject,
+  getAllProjects
 } = require("../controllers/project_controller");
 
 const projectRoutes = (app) => {
@@ -11,6 +13,8 @@ const projectRoutes = (app) => {
   router.post("/add-project", createProject);
   router.delete("/delete-project/:id", deleteOneProject);
   router.delete("/delete-all-projects", deleteAllProjects);
+  router.get("/get-project/:id", getOneProject);
+  router.get("/get-all-projects", getAllProjects)
 
   app.use("/api/projects", router);
 };
