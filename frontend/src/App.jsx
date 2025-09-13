f753830ae95fe99c1014a0781381a6a01ca26772
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-13 11:08:51 +0000

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-green-500 font-bold underline">In Progres</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
