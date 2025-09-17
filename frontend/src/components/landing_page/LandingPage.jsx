bf961f64aa31febf8398fa2635ca5a732a1d7a4e
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-17 07:19:37 +0000

    create nav

--- frontend/src/components/landing_page/LandingPage.jsx
+++ frontend/src/components/landing_page/LandingPage.jsx
@@ -2,12 +2,16 @@
 
 function LandingPage() {
   return (
-    <div className="bg-gray-900 h-full">
+    <div className="bg-gray-900 h-full p-4">
       <nav>
         <div className="flex items-center justify-between">
-          <img src="https://i.postimg.cc/YSrTM4XF/devlink-logo-removebg-preview.png" width="50" alt="" />
-          <div className="justify-end"><a href="">Login</a></div>
+          <img src="https://i.postimg.cc/wxSVR0jG/logo.png" className="rounded-sm" width="50" alt="logo" />
+          <div className="justify-end text-white hover:text-blue-500"><a href="">Login</a></div>
         </div>
+
+        <main className="border-red-500 m-10">
+          <h1 className="text-3xl">Build Your Portfolio. Grow Your Network. Land Your First Dev Job.</h1>
+        </main>
       </nav>
     </div>
   )
