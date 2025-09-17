aba5ed3810587b0afbe57fa1dd549e204e090c72
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-09-17 15:13:39 +0000

    icrease icon size

--- frontend/src/components/landing_page/LandingPage.jsx
+++ frontend/src/components/landing_page/LandingPage.jsx
@@ -22,10 +22,10 @@ function LandingPage() {
         <article className="mt-10">
           <h2 className="text-2xl text-white">Key Features</h2>
           <ul className="grid grid-cols-4 p-2 text-left text-white mt-2">
-            <li><span className="text-primary-variant"><strong><FaClipboardList />Project Management, Simplified:</strong></span> Organize your projects and ideas in one place. No more scattered files—just a clean, professional portfolio.</li>
-            <li><span className="text-primary-variant"><strong><FcCollaboration />Real-Time Teamwork:</strong></span> Work with other developers in a secure, shared environment. Practice your collaboration skills and build something bigger than yourself.</li>
-            <li><span className="text-primary-variant"><strong><FaGithub />Automatic Proof of Work:</strong></span> Connect your GitHub to instantly display your activity. Show employers you’re serious about your craft.</li>
-            <li><span className="text-primary-variant"><strong><IoIosPeople />Find Your People:</strong></span> Connect with a community of other early-career developers who are facing the same challenges. Share insights and support each other.</li>
+            <li><span className="text-primary-variant"><strong><FaClipboardList size={30} />Project Management, Simplified:</strong></span> Organize your projects and ideas in one place. No more scattered files—just a clean, professional portfolio.</li>
+            <li><span className="text-primary-variant"><strong><FcCollaboration size={30} />Real-Time Teamwork:</strong></span> Work with other developers in a secure, shared environment. Practice your collaboration skills and build something bigger than yourself.</li>
+            <li><span className="text-primary-variant"><strong><FaGithub size={30} />Automatic Proof of Work:</strong></span> Connect your GitHub to instantly display your activity. Show employers you’re serious about your craft.</li>
+            <li><span className="text-primary-variant"><strong><IoIosPeople size={30} />Find Your People:</strong></span> Connect with a community of other early-career developers who are facing the same challenges. Share insights and support each other.</li>
           </ul>
 
           <button className="text-white bg-primary p-2 rounde-50">Start Building Your Future</button>
