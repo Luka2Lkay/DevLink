9df5ff745c21bbb8b107ac56ae346fb5751d4244
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-10-01 09:52:07 +0000

    register template

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import axios from "axios"
 
 function Register() {
   const initialFormFields = {
@@ -10,6 +11,7 @@ function Register() {
   }
 
   const [register, setRegister] = useState(initialFormFields);
+  const [error, setError] = useState({ password: "", email: "", githubUsername: "", server: "" });
 
   const handleChange = (e) => {
     setRegister({
@@ -19,12 +21,28 @@ function Register() {
   }
 
   const signUp = async (e) => {
-    e.prevent.default();
+    e.preventDefault();
 
     const data = { ...register };
     const baseUrl = "http://localhost:3000/api/users/signup";
 
-    await axios.post(baseUrl, data).then(reset())
+
+    if (data.password !== data.confirmPassword) {
+      return setError({ ...error, password: "Passwords don't match" });
+    }
+
+    try {
+
+      await axios.post(baseUrl, data)
+
+      reset();
+    } catch (error) {
+      if (error.response) {
+        setError({ ...error, server: error.response.data.message });
+      } else {
+        setError({ ...error, server: "An error occurred. Please try again." });
+      }
+    }
   }
 
   const reset = () => {
@@ -36,41 +54,59 @@ function Register() {
 
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img src="https://i.postimg.cc/wxSVR0jG/logo.png" alt="logo" className="mx-auto w-auto h-10 rounded-sm" />
-        <h2 className="mt-10 text-white text-center font-bold">Sign in to your account</h2>
+        <h2 className="mt-10 text-white text-center font-bold">Sign up</h2>
       </div>
+
+
+      <p className="text-red-500 mt-2 text-sm/6">{error.server}</p>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form onSubmit={signUp} className="space-y-6">
 
           <div>
+            <label htmlFor="name" className="text-left text-gray-100 block font-medium text-sm/6">Name</label>
+            <div className="mt-2">
+              <input id="name" name="name" type="text" value={register.name} onChange={handleChange} autoComplete="name" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 -outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:-outline-offset-2 sm:text-sm/6" />
+            </div>
+          </div>
+
+          <div>
             <label htmlFor="email" className="text-left text-gray-100 block font-medium text-sm/6">Email address</label>
             <div className="mt-2">
-              <input id="email" name="email" type="email" value={login.email} onChange={handleChange} autoComplete="email" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 -outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:-outline-offset-2 sm:text-sm/6" />
+              <input id="email" name="email" type="email" value={register.email} onChange={handleChange} autoComplete="email" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 -outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:-outline-offset-2 sm:text-sm/6" />
             </div>
           </div>
 
           <div>
             <label htmlFor="password" className="text-left text-gray-100 block font-medium text-sm/6">Password</label>
             <div className="mt-2">
-              <input id="password" name="password" type="password" value={login.password} onChange={handleChange} autoComplete="none" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2 sm:text-sm/6" />
-            </div>
-          </div>
-
-         <div>
-            <label htmlFor="confirm-password" className="text-left text-gray-100 block font-medium text-sm/6">Confirm Password</label>
-            <div className="mt-2">
-              <input id="confirm-password" name="confirmPassword" type="password" value={register.confirmPassword} onChange={handleChange} autoComplete="none" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2 sm:text-sm/6" />
+              <input id="password" name="password" type="password" value={register.password} onChange={handleChange} autoComplete="new-password" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2 sm:text-sm/6" />
             </div>
           </div>
 
           <div>
-            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm/6 text-white font-semibold hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:ouline-indigo-500 ">Sign in</button>
+            <label htmlFor="confirm-password" className="text-left text-gray-100 block font-medium text-sm/6">Confirm Password</label>
+            <div className="mt-2">
+              <input id="confirm-password" name="confirmPassword" type="password" value={register.confirmPassword} onChange={handleChange} autoComplete="new-password" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:outline-offset-2 sm:text-sm/6" />
+              <p className="text-red-500 mt-2 text-sm/6">{error.password}</p>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="github-username" className="text-left text-gray-100 block font-medium text-sm/6">Github Username</label>
+            <div className="mt-2">
+              <input id="github-username" name="githubUsername" type="text" value={register.githubUsername} onChange={handleChange} autoComplete="github-username" required className="block w-full px-3 py-2 bg-white/5 text-base text-white outline-1 -outline-offset-1 outline-white/10 rounded-md shadow-sm placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-500 focus:-outline-offset-2 sm:text-sm/6" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm/6 text-white font-semibold hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:ouline-indigo-500 ">Sign up</button>
           </div>
 
         </form>
 
-        <p className="mt-10 text-center `text-sm/6 text-gray-400">
-          Don't have an account? <a href="#" className="font-semibold text-indigo-500 hover:text-indigo-400">Sign up</a>.
+        <p className="mt-10 text-center text-sm/6 text-gray-400">
+          Already have an account? <a href="#" className="font-semibold text-indigo-500 hover:text-indigo-400">Sign in</a>.
         </p>
 
       </div>
