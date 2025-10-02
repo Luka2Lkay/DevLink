bf13c55c67ad2907cc1e32abf4b509d3f59e35f0
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-10-02 14:56:40 +0000

    should load and display the loading spinner when submitting

--- frontend/src/components/register/Register.test.jsx
+++ frontend/src/components/register/Register.test.jsx
@@ -10,6 +10,34 @@ describe("Register Component", () => {
     render(<Register />);
   });
 
+  afterEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  it("should load and display the loading spinner when submitting", async () => {
+    axios.post.mockImplementation(() => new Promise(() => {}));
+
+    const nameInput = screen.getByTestId("name-id");
+    const emailInput = screen.getByTestId("email-id");
+    const passwordInput = screen.getByTestId("password-id");
+    const confirmPasswordInput = screen.getByTestId("confirm-password-id");
+    const githubUsernameInput = screen.getByTestId("github-username-id");
+    const signUpButton = screen.getByRole("button", { name: /sign up/i });
+
+    await userEvent.type(nameInput, "Luka");
+    await userEvent.type(emailInput, "luka@gmail.com");
+    await userEvent.type(passwordInput, "password");
+    await userEvent.type(confirmPasswordInput, "password");
+    await userEvent.type(githubUsernameInput, "luka-git");
+
+    await userEvent.click(signUpButton);
+
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledTimes(1);
+      expect(screen.getByRole("progressbar")).toBeInTheDocument();
+    });
+  });
+
   it("should display error message when there's an error", async () => {
     axios.post.mockRejectedValue(new Error("Network Error"));
 
@@ -27,15 +55,14 @@ describe("Register Component", () => {
     await userEvent.type(githubUsernameInput, "luka-git");
 
     await userEvent.click(signUpButton);
-    
-    await waitFor(() => {   
-        expect(axios.post).toHaveBeenCalledTimes(1);
-        expect(
-          screen.getByText("An error occurred. Please try again.")
-        ).toBeInTheDocument();
-    })
 
-});
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledTimes(1);
+      expect(
+        screen.getByText("An error occurred. Please try again.")
+      ).toBeInTheDocument();
+    });
+  });
 
   it("has all input fields", () => {
     expect(screen.getByLabelText("Name")).toBeInTheDocument();
