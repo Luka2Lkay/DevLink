456824512cb71d3b125d73ef301119be4888e90b
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-10-02 12:20:10 +0000

    should have a submit button

--- frontend/src/components/register/Register.test.jsx
+++ frontend/src/components/register/Register.test.jsx
@@ -1,28 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Register from "./Register";
 
 describe("Register Component", () => {
-    it("renders the register form", () => {
-        render(<Register />);
-    });
+  beforeEach(() => {
+    render(<Register />);
+  });
 
-    it("has all input fields", () => {
-        const { getByLabelText } = render(<Register />);
-        expect(getByLabelText("Name")).toBeInTheDocument();
-        expect(getByLabelText("Email address")).toBeInTheDocument();
-        expect(getByLabelText("Password")).toBeInTheDocument();
-        expect(getByLabelText("Confirm Password")).toBeInTheDocument();
-        expect(getByLabelText("Github Username")).toBeInTheDocument();
-    });
+  it("has all input fields", () => {
+    expect(screen.getByLabelText("Name")).toBeInTheDocument();
+    expect(screen.getByLabelText("Email address")).toBeInTheDocument();
+    expect(screen.getByLabelText("Password")).toBeInTheDocument();
+    expect(screen.getByLabelText("Confirm Password")).toBeInTheDocument();
+    expect(screen.getByLabelText("Github Username")).toBeInTheDocument();
+  });
 
-    it("has a submit button", () => {
-        const { getByRole } = render(<Register />);
-        expect(getByRole("button", { name: /sign up/i })).toBeInTheDocument();
-    });
+  it("has a submit button", () => {
+    expect(
+      screen.getByRole("button", { name: /sign up/i })
+    ).toBeInTheDocument();
+  });
 
-    it("has a link to sign in", () => {
-        const { getByText } = render(<Register />);
-        expect(getByText(/already have an account\?/i)).toBeInTheDocument();
-    });
-
+  it("has a link to sign in", () => {
+    expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument();
+  });
 });
