f9fa5d2f25b1177d88fb04c2221b439fa2dd2108
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-10-07 12:19:01 +0000

    refactor code

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
 const { db } = require("./src/config/db_config");
 const { userRoutes } = require("./src/routes/user_routes");
 const { projectRoutes } = require("./src/routes/project_routes");
 const { inviteRoutes } = require("./src/routes/invite_routes");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "https://super-duper-robot-9q5jrvq5vjjhp96q-5173.app.github.dev/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "https://super-duper-robot-9q5jrvq5vjjhp96q-5173.app.github.dev",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200,
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 mongoose
   .connect(db.mongoDbUrl)
