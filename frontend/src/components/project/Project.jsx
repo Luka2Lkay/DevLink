2b3b81819a61ae1861cc9137a379dcdf4092ad29
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-10-14 09:12:52 +0000

    project card template

--- frontend/src/components/project/Project.jsx
+++ frontend/src/components/project/Project.jsx
@@ -1,3 +1,6 @@
+import { v4 as uuidv4 } from 'uuid';
+import Collaborator from '../collaborator/Collaborator';
+
 function Project({ title, description, owner, collaborators = [] }) {
   return (
     <div className="max-w-sm mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl transform hover:scale-[1.02] transition duration-3000 ease-in-out">
@@ -12,10 +15,10 @@ function Project({ title, description, owner, collaborators = [] }) {
         </p>
 
         <div className="mt-4 pt-4 border-t border-gray-200">
-          <div className="flex items-center mb-3">
-            <span className="text-sx font-semibold uppercase tracking-wider text-indigo-600 mr-2">
-              Owner:
-            </span>
+          <span className="text-xs font-semibold uppercase tracking-wider text-indigo-600">
+            Owner:
+          </span>
+          <div className='mb-2'>
             <span className="text-sm font-medium text-gray-700">
               {owner || "Owner Name"}
             </span>
@@ -26,10 +29,13 @@ function Project({ title, description, owner, collaborators = [] }) {
               Collaborators:
             </span>
             <div className="flex -space-x-2 overflow-hidden">
-              {collaborators.map((collaborator, index) => (
-
-                <p>hey</p>
-              ))}
+              {collaborators.length === 0 ? (
+                <span className="w-full text-sm text-gray-500 flex justify-center items-center">No collaborators</span>
+              ) : (
+                collaborators.map((collaborator) => (
+                  <Collaborator key={uuidv4()} initials={collaborator.initials} />
+                ))
+              )}
             </div>
           </div>
         </div>
