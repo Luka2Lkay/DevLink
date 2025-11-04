9fdba6dd82ce32faa70f9f263b338241b630fb1f
Luka <luka.matshebelele@gmail.com>
2025-11-04 17:42:50 +0200

    uncomment code

--- frontend/src/state/thunks/invite_thunk.js
+++ frontend/src/state/thunks/invite_thunk.js
@@ -6,15 +6,15 @@ export const sendInviteThunk = createAsyncThunk(
 
         try {
             console.log(id)
-            // const user = sessionStorage.getItem("user");
-            // const response = await axios.post(`https://devlink-9xp4.onrender.com/api/invites/send-invite/${id}`, email, {
-            //     headers: {
-            //         Authorization: `Bearer ${JSON.parse(user).token}`
-            //     }
-            // })
+            const user = sessionStorage.getItem("user");
+            const response = await axios.post(`https://devlink-9xp4.onrender.com/api/invites/send-invite/${id}`, email, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(user).token}`
+                }
+            })
 
-            // console.log(response.data);
-            // return response.data;
+            console.log(response.data);
+            return response.data;
 
         } catch (error) {
             return rejectWithValue("Failed to send an invite", error.response.data)
