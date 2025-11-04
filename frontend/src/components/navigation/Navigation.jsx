dbd36ea57a0ae27a2d48d4c2114e27b1491334ee
Luka <luka.matshebelele@gmail.com>
2025-11-04 12:46:21 +0200

    Projects link

--- frontend/src/components/navigation/Navigation.jsx
+++ frontend/src/components/navigation/Navigation.jsx
@@ -1,15 +1,10 @@
 import { Link } from "react-router-dom"
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function Navigation() {
     return (
-        <div className="flex justify-between text-red-500 mb-2">
+        <div className="flex justify-end gap-2 text-red-500 mb-2">
 
-            <ArrowBackIcon
-                aria-label="back-button"
-                className="text-white m-5 text-3xl cursor-pointer"
-                onClick={() => window.history.back()}
-            />
+            <Link to={"/feed"}>Projects</Link>
 
             <Link to={"/"} onClick={() => sessionStorage.clear()}>Logout</Link>
         </div>
