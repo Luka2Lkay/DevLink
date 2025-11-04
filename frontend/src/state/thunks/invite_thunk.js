b83a266ffdc73649532001364805d624f70f5796
Luka <luka.matshebelele@gmail.com>
2025-11-04 17:35:43 +0200

    work on sendInviteThunk

--- frontend/src/state/thunks/invite_thunk.js
+++ frontend/src/state/thunks/invite_thunk.js
@@ -2,18 +2,19 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios"
 
 export const sendInviteThunk = createAsyncThunk(
-    "invite/sendInvite", async (projectData, { rejectWithValue }) => {
+    "invite/sendInvite", async (id, email, { rejectWithValue }) => {
 
         try {
-            const user = sessionStorage.getItem("user");
-            const response = await axios.post(`https://devlink-9xp4.onrender.com/api/invites/send-invite/${projectData.id}`, projectData, {
-                headers: {
-                    Authorization: `Bearer ${JSON.parse(user).token}`
-                }
-            })
+            console.log(id)
+            // const user = sessionStorage.getItem("user");
+            // const response = await axios.post(`https://devlink-9xp4.onrender.com/api/invites/send-invite/${id}`, email, {
+            //     headers: {
+            //         Authorization: `Bearer ${JSON.parse(user).token}`
+            //     }
+            // })
 
-            console.log(response.data);
-            return response.data;
+            // console.log(response.data);
+            // return response.data;
 
         } catch (error) {
             return rejectWithValue("Failed to send an invite", error.response.data)
