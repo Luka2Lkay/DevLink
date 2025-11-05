4bd1cf1b0a44bdf4f0777d4a35bac331307fa428
Luka <luka.matshebelele@gmail.com>
2025-11-05 13:11:43 +0200

    check status

--- frontend/src/state/thunks/invite_thunk.js
+++ frontend/src/state/thunks/invite_thunk.js
@@ -5,20 +5,20 @@ export const sendInviteThunk = createAsyncThunk(
     "invite/sendInvite", async ({ id, email }, { rejectWithValue }) => {
 
         try {
-            const user = sessionStorage.getItem("user");
+            const userString = sessionStorage.getItem("user");
 
-            if (!user) rejectWithValue("User not authenticated!")
+            if (!userString) rejectWithValue("User not authenticated!")
 
-            const { token } = JSON.parse(user);
+            const { token } = JSON.parse(userString);
             const response = await axios.post(`https://devlink-9xp4.onrender.com/api/invites/send-invite/${id}`, { email }, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             })
 
-            if (response.status === 409 || response.status === 400 || response.status === 401 || response.status === 404) {
-                return response.data.message;
-            }
+            // if (response.status === 409 || response.status === 400 || response.status === 401 || response.status === 404) {
+            //     return response.data.message;
+            // }
 
             return response.data;
         } catch (error) {
