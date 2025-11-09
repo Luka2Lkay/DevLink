68add313a038bcdb82b2ba6b49963f99f6fbce00
Luka <luka.matshebelele@gmail.com>
2025-11-09 09:39:46 +0200

    work on NotificationsList

--- frontend/src/components/notifications_list/NotificationsList.jsx
+++ frontend/src/components/notifications_list/NotificationsList.jsx
@@ -1,15 +1,32 @@
-
+import Navigation from "../navigation/Navigation";
+import { recievedInvites } from "../../state/thunks/invite_thunk";
+import { selectInvites, removeInvite } from "../../state/reducers/invite_slice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function NotificationsList() {
+  const dispatch = useDispatch();
+  const notitifications = useSelector(selectInvites) ?? [];
+
+  useEffect(() => {
+    dispatch(recievedInvites());
+  });
+
   return (
     <div>
- <button className="text-white">All</button>
+      <Navigation />
+      <button className="text-white">All</button>
 
- <div className="flex">
-<p className>First Notification</p>
- </div>
+      <div className="flex">
+        {notitifications.length > 0 &&
+          notitifications.map((notification) => (
+            <div className="flex">
+              <p className="text-white">{notification.fromUser.name}</p>
+            </div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NotificationsList
+export default NotificationsList;
