d29981b00734be7063cbd54e59c8cf9343ef5243
Luka <luka.matshebelele@gmail.com>
2025-11-15 09:33:03 +0200

    modify collaborator props

--- frontend/src/components/collaborator/Collaborator.test.jsx
+++ frontend/src/components/collaborator/Collaborator.test.jsx
@@ -3,12 +3,12 @@ import Collaborator from './Collaborator';
 
 describe('Collaborator', () => {
     it('should render the initials correctly', () => {
-        render(<Collaborator initials="AB" />);
-        expect(screen.getByText('AB')).toBeInTheDocument();
+        render(<Collaborator name="username" />);
+        expect(screen.getByText('username')).toBeInTheDocument();
     });
 
     it('should correct styles applied', () => {
-        render(<Collaborator initials="CD" />);
-        expect(screen.getByText('CD')).toHaveClass('inline-flex items-center justify-center h-8 w-8 rounded-full bg-blue-100 text-sm font-medium text-blue-800 ring-2 ring-white');
+        render(<Collaborator name="username" />);
+        expect(screen.getByText('username')).toHaveClass('inline-flex items-center justify-center h-8 w-8 rounded-full bg-blue-100 text-sm font-medium text-blue-800 ring-2 ring-white');
     });
 });
