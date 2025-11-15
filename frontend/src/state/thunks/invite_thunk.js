8374c8217c89c9864156b4134c9384eade3617db
Luka <luka.matshebelele@gmail.com>
2025-11-15 15:38:17 +0200

    change the output of the thunk

--- frontend/src/state/thunks/invite_thunk.js
+++ frontend/src/state/thunks/invite_thunk.js
@@ -28,7 +28,7 @@ export const sendInviteThunk = createAsyncThunk(
         }
       );
 
-      return response.data.message;
+      return response.data
     } catch (error) {
       return rejectWithValue(
         error.response?.data?.message || "Failed to send an invite"
