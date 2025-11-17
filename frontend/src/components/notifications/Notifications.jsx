945c53385b177dc251acdc19b80f58579ba521ed
Luka <luka.matshebelele@gmail.com>
2025-11-17 09:36:00 +0200

    remove commented lines of code

--- frontend/src/components/notifications/Notifications.jsx
+++ frontend/src/components/notifications/Notifications.jsx
@@ -1,8 +1,6 @@
 import Badge from "@mui/material/Badge";
 import IconButton from "@mui/material/IconButton";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-// import Menu from "@mui/material/Menu";
-// import MenuItem from "@mui/material/MenuItem";
 import { receivedInvitesThunk } from "../../state/thunks/invite_thunk";
 import { selectInvites } from "../../state/reducers/invite_slice";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,20 +12,14 @@ function Notifications() {
   const navigate = useNavigate();
 
   const notifications = useSelector(selectInvites) ?? [];
-  // const [anchorElement, setAnchorElement] = useState(null);
-  // const open = Boolean(anchorElement);
 
-  const handleClick = (e) => {
+  const handleClick = () => {
     navigate("/notifications-list");
   };
 
   useEffect(() => {
     dispatch(receivedInvitesThunk());
   }, [dispatch]);
-
-  // const handleClose = () => {
-  //   setAnchorElement(null);
-  // };
 
   const numberOfUnhandledInvites = () => {
     const pendingInvites = notifications.filter(
@@ -36,11 +28,6 @@ function Notifications() {
 
     return pendingInvites.length;
   };
-
-  // const handleMarkAsRead = (id) => {
-  //   //Consider reworking this
-  //   dispatch(removeInvite(id));
-  // };
 
   return (
     <>
@@ -53,27 +40,6 @@ function Notifications() {
           <NotificationsIcon />
         </Badge>
       </IconButton>
-
-      {/* <Menu
-        id="notification-menu"
-        open={open}
-        anchorEl={anchorElement}
-        onClose={handleClose}
-        slotProps={{ list: { "aria-labelledby": "notification-button" } }}
-      >
-        {notifications.length > 0 ? (
-          notifications.map((notification) => (
-            <MenuItem
-              key={notification.id}
-              onClick={() => handleMarkAsRead(notification.id)}
-            >
-              <p>New invite! from {notification.fromUser.name}</p>
-            </MenuItem>
-          ))
-        ) : (
-          <MenuItem onClick={handleClose}>No new notifications</MenuItem>
-        )}
-      </Menu> */}
     </>
   );
 }
