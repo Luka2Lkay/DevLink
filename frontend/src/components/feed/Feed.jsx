8accbb6ef22b9c228e8222a8dac1332aace4c2b5
Luka <luka.matshebelele@gmail.com>
2025-11-20 18:24:07 +0200

    fix error

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -75,7 +75,7 @@ function Feed() {
       } else {
         const result = await dispatch(addProjectThunk(project));
         dispatch(addProject(project));
-        consol
+      
         if (result.payload) {
           await dispatch(setErrorMessage(result.payload));
           console.log('Error adding project:', result.payload);
