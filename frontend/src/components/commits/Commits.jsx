1c5c785c77cce4682b9ca994b938a7927652586e
Luka <luka.matshebelele@gmail.com>
2025-11-30 18:35:09 +0200

    display commits

--- frontend/src/components/commits/Commits.jsx
+++ frontend/src/components/commits/Commits.jsx
@@ -1,15 +1,18 @@
 import Navigation from "../navigation/Navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchProjectCommitsThunk } from "../../state/thunks/project_thunk";
 import { useParams } from "react-router-dom";
+import SingleCommit from "../single_commit/SingleCommit";
 
 function Commits() {
   const dispatch = useDispatch();
-  const {id}  = useParams();
+  const { id } = useParams();
+
+  const [commits, setCommits] = useState([]);
 
   useEffect(() => {
-    dispatch(fetchProjectCommitsThunk(id));
+    setCommits(dispatch(fetchProjectCommitsThunk(id)));
   }, []);
 
   return (
@@ -17,6 +20,20 @@ function Commits() {
       <Navigation />
       <div className="p-4">
         <h1 className="text-2xl text-white font-bold mb-4">Commits</h1>
+        {commits && commits.length > 0 ? (
+          commits.map((commit) => (
+            <SingleCommit
+              author={commit.author}
+              date={commit.date.split(":")[0]}
+              message={commit.message}
+              sha={commit.sha}
+              url={commit.url}
+              key={commit.sha}
+            />
+          ))
+        ) : (
+          <p className="text-white">No commits available for this project.</p>
+        )}
       </div>
     </div>
   );
