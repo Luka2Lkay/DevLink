26ebaa9f139ba04b10276e16b40e61119d3ed36c
Luka <luka.matshebelele@gmail.com>
2025-12-01 10:01:27 +0200

    register commit reducer

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectReducer from './reducers/project_slice';
 import inviteReducer from './reducers/invite_slice'
+import commiteReducer from './reducers/commit_slice';
 
 const store = configureStore({
     reducer: {
         project: projectReducer,
-        invite: inviteReducer
+        invite: inviteReducer,
+        commit: commiteReducer,
     },
 });
 
